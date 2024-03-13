20a2cc2f98d49aec0be7e5f0a4c9579623a7c3d7
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2024-03-13 19:16:43 +0200

    stream example

---
+++ Lecture_6_streams/src/terminal/FilteringStream.java
@@ -0,0 +1,21 @@
+package terminal;
+
+import java.util.Collection;
+import java.util.List;
+
+public class FilteringStream {
+    public static void main(String[] args) {
+        List<String> elements = List.of("test2", "test", "test", "xtest");
+        elements
+                .stream()
+                .filter(el -> el.startsWith("t"))
+                .map(el -> capitalize(el))
+                .sorted()
+                .peek(el -> System.out.println(el));
+//                .toList();
+    }
+
+    private static String capitalize(String el) {
+        return el.isEmpty() ? el : el.substring(0, 1).toUpperCase() + el.substring(1);
+    }
+}
