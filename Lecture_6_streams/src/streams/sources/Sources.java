65e02116b922f22585ba46b5798e572cc06f9a90
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2024-03-14 19:25:16 +0200

    sources

--- Lecture_6_streams/src/streams/sources/Sources.java
+++ Lecture_6_streams/src/streams/sources/Sources.java
@@ -1,9 +1,24 @@
 package streams.sources;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class Sources {
     public static void main(String[] args) {
         List.of("test", "test2").forEach(el -> System.out.println("test"));
+        Stream<String> random = Stream.generate(() -> "r: " + Math.random());
+        System.out.println(random.findFirst());
+
+//        System.out.println(random.collect(Collectors.joining()));
+
+//        IntStream intStream = IntStream.of(1, 2, 3);
+//        System.out.println(intStream.sum());
+
+//        String str = "hello\ntest";
+//        Stream<String> lines = str.lines();
+//        lines.forEach(System.out::println);
     }
 }
