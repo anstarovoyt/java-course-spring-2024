c19a6c8b64e3cbf8d740ad1aa9c88eb91c833e24
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2024-03-15 09:12:17 +0200

    custom functions examples

---
+++ Lecture_6_streams/src/functions/CustomFunctions.java
@@ -0,0 +1,35 @@
+package functions;
+
+public class CustomFunctions {
+    public static void main(String[] args) {
+        String hello = "hello";
+        process(hello, System.out::println);
+        processString(hello, System.out::println);
+    }
+
+    private static <T> void process(T el, Processor<T> processor) {
+        processor.process(el);
+    }
+
+    private static void processString(String el, StringProcessor processor) {
+        processor.process(el);
+    }
+
+    @FunctionalInterface
+    interface Processor<T> {
+        void process(T el);
+
+        default String name() {
+            return "processor";
+        }
+    }
+
+    interface StringProcessor extends Processor<String> {
+        @Override
+        default void process(String el) {
+            processString(el);
+        }
+
+        void processString(String el);
+    }
+}
