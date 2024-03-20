4cdac072f45eefdbd5a2e77ddbe803811a46b377
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2024-03-20 19:49:52 +0200

    data sharing

---
+++ Lecture_7_threading/src/data/SharingThreads.java
@@ -0,0 +1,21 @@
+package data;
+
+public class SharingThreads {
+
+    private static boolean flag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            while (!flag) {
+            }
+            System.out.println("Thread 1 seen the flag");
+        }).start();
+
+        Thread.sleep(100);
+
+        new Thread(() -> {
+            flag = true;
+            System.out.println("Thread 2 set the flag");
+        }).start();
+    }
+}
---
+++ Lecture_7_threading/src/data/SimpleBlockingQueue.java
@@ -0,0 +1,52 @@
+package data;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimpleBlockingQueue {
+    private final Deque<Integer> queue = new ArrayDeque<>();
+
+    public synchronized void put(int value) throws InterruptedException {
+        queue.add(value);
+        notify();
+    }
+
+    public synchronized int get() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        int value = queue.poll();
+        notify();
+        return value;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue queue = new SimpleBlockingQueue();
+
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                int value = (int) Math.round(Math.random() * 10);
+                try {
+                    queue.put(value);
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Produced " + value);
+
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                try {
+                    System.out.println("Consumed " + queue.get());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }).start();
+    }
+}
+
